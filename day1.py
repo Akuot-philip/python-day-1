fa4abfff25f60ee1a454e5899623ecf30c3a7294
Akuot-philip <akuotphilipp@gmail.com>
2022-05-09 16:27:34 +0300

    day-1-python challenge

---
+++ day1.py
@@ -0,0 +1,33 @@
+# Day 1 - 30DaysOfPython Challenge
+#Open the python interactive shell and do the following operations. The operands are 3 and 4.
+print(3+4)     #addition(+)
+print(3-4)     #subtraction(-)
+print(3*4)     #multiplication(*)
+print(3%4)     #modulus(%)
+print(3/4)     #division(/)
+print(3**4)    #exponential(**)
+print(3//4)    #floor division operator(//)
+
+#Write strings on the python interactive shell. The strings are the following:
+name ="Akuot Philip"
+print (name)
+familyName ="Chol Anyieth"
+print (familyName)
+myCountry = "South sudan"
+print (myCountry)
+
+#Check the data types of the following data:
+print(type(10)) 
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type(name))
+print(type(familyName))
+print(type(myCountry))
+print(type((1,2,3,4,5)))
+print(type({1,2,3,4,5}))
+print(type(True))
+print(type(False))
+print(type({'name':'Akuot'}))
+
